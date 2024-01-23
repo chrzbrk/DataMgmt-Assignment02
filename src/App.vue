4744f2bf508466ddb003ea0e04b413e88be4efb4
chrzbrk <156067510+chrzbrk@users.noreply.github.com>
2024-01-23 13:17:07 +0100

    Added Exercise4

--- src/App.vue
+++ src/App.vue
@@ -36,14 +36,24 @@ function resetTimer(){
 // (3) Add an input text in the template and a button with @click. Once the button is clicked, it should call back a function that adds the text to a ref variable and display it
 
 var added_Text = ref("No text added so far!");
-var input_Text = ref("")
+var input_Text = ref("");
 
 function addText(){
     if(added_Text.value === "No text added so far!"){
         added_Text.value = "";
     }
-    added_Text.value = added_Text.value + input_Text.value
+    added_Text.value = added_Text.value + input_Text.value;
 
+}
+
+
+// (4) Add two input texts in the sidebar and a button with @click. One will collect a selector and the second one a color. Once youpress the button, it should change the color to that css selector.
+var color_selection = ref("");
+var element_selection = ref("");
+
+function changeStyle() {
+    var element = document.querySelector(element_selection.value);
+    element.style.backgroundColor = color_selection.value;
 }
 
 </script>
@@ -67,7 +77,10 @@ function addText(){
             <p></p>
             <input type="text" id="text_box" v-model="input_Text" name="text_box" placeholder="Put your Text here!">
             <button @click="addText">Add Text</button>
-
+            <p></p>
+            <input type="text" id="change_color" v-model="color_selection" name="text_box" placeholder="Write Color">
+            <input type="text" id="change_element" v-model="element_selection" name="text_box" placeholder="Write Elemente">
+            <button @click="changeStyle">Change Style</button>
         </div>
 
         <div id="main" class="container">
@@ -89,18 +102,14 @@ html{
 }
 
 body{
-
     margin:0;
 }
 
 div{
-
-    /* put the borders in the inside of container */
     box-sizing: border-box;
 }
 
 img{
-
     height: 100%;
     width: auto;
 }
@@ -116,13 +125,11 @@ img{
 }
 
 #flex{
-    
     display: flex;
     height: calc(100vh - 50px);
 }
 
 #sidebar{
-    
     width:30%;
     border-color: blue;
 }
@@ -140,7 +147,6 @@ button{
 }
 
 .container{
-
     border-style: dotted;
     border-width: 1px;
 }
